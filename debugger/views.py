a3c541258735cf01bd4cee2c4d0f91b59f2353bd
germain <katakugermain@gmail.com>
2019-01-05 14:22:19 +0100

    add view in app debugger

--- debugger/views.py
+++ debugger/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
 from .models import Debugger
 
 # Create your views here.
-class DebuggerList(ListView):
-    model = Debugger
-    template_name = 'debugger/debugger_list.html'
 
-# def debug(request):
-#     return render(request, 'debugger/debugger_list.html')
-    
+def debug_list(request):
+    debugger_list = Debugger.objects.all()
+    template_name = 'debugger/debugger_list.html'
+    context = {
+        'debugger_list': debugger_list
+    }
+    return render(request, template_name, context )
+
+def debug_detail(request, debug_id):
+    debugger = get_object_or_404(Debugger, id=debug_id)
+    template_name = 'debugger/debugger_details.html'
+    context = {
+        'debugger': debugger
+    }
+    return render(request, template_name, context)
